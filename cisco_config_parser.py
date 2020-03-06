846f52eb64752ae297515d7b9e59df17d2d01c5a
kperep <kperep@gmail.com>
2020-03-06 14:39:56 +0700

    Добавлена авторизация на Cisco

--- cisco_config_parser.py
+++ cisco_config_parser.py
@@ -5,21 +5,24 @@ import time
 
 Host = "vpn.kansk-tc.ru"
 
-# user=input("Enter User name")
-# password=getpass.getpass()
+user = 'cisco'
+password = 'cisco'
 
 tn = telnetlib.Telnet(Host, 32771, 5)
-# tn.read_until("Username: ")
-# tn.write(user.encode('ascii') + b"\n")
-
-# if password:
-# tn.read_until(b”Password: “)
-# tn.write(password.encode(‘ascii’)+b”\n”)
 tn.write(b"\n")
 time.sleep(2)
+#tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+
+time.sleep(2)
+if password:
+#    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii')+b"\n")
+time.sleep(1)
 tn.write(b"enable\n")
-time.sleep(2)
+time.sleep(1)
 tn.write(b"show run\n")
-time.sleep(2)
-line = tn.read_until(b'no mmi auto-configure')
+time.sleep(1)
+line = tn.read_until(b'aaa authorization console')
 print(line)
+tn.write(b"exit\n")
