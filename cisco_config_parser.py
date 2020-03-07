af7c48d5ad1ae20e10cc4cded3220a728041e351
kperep <kperep@gmail.com>
2020-03-08 01:10:29 +0700

    Добавлена адекватная авторизация и считывание.

--- cisco_config_parser.py
+++ cisco_config_parser.py
@@ -5,24 +5,20 @@ import time
 
 Host = "vpn.kansk-tc.ru"
 
-user = 'cisco'
-password = 'cisco'
+user = "cisco"
+password = "cisco"
 
-tn = telnetlib.Telnet(Host, 32771, 5)
-tn.write(b"\n")
-time.sleep(2)
-#tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-
-time.sleep(2)
+tn = telnetlib.Telnet(Host, 32771)
+tn.write(b"\r")
+tn.read_until(b"Username:")
+tn.write(user.encode() + b"\r")
 if password:
-#    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii')+b"\n")
-time.sleep(1)
+    tn.read_until(b"Password:")
+    tn.write(password.encode()+b"\r")
 tn.write(b"enable\n")
-time.sleep(1)
 tn.write(b"show run\n")
-time.sleep(1)
-line = tn.read_until(b'aaa authorization console')
+line = tn.read_until(b'bratishka', 1)
+tn.write(b"\n")
+tn.write(b"quit\n")
 print(line)
-tn.write(b"exit\n")
+
